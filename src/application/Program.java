f50689a8dd2dc66722614e04150060c0ed20e130
Cristhyan Couto <cristhyancouto@gmail.com>
2023-08-21 19:28:17 -0300

    versao 1.0.0

---
+++ src/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		int aux = 1;
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		Client client = new Client();
+
+		client.register();
+		System.out.println("");
+
+		while (aux == 1 || aux == 2) {
+			System.out.println("What would you like to do?");
+			System.out.println("Type 1 for deposit.");
+			System.out.println("Type 2 for withdraw.");
+			System.out.println("Any other option will close the operation.");
+			
+			aux = sc.nextInt();
+
+			switch (aux) {
+			case 1:
+				System.out.println("Type de value: ");
+				client.deposit(sc.nextDouble());
+				System.out.println("");
+				System.out.println(client.toString());
+				System.out.println("");
+				break;
+			case 2:
+				System.out.println("Type de value: ");
+				client.withdraw(sc.nextDouble());
+				System.out.println("");
+				System.out.println(client.toString());
+				System.out.println("");
+				break;
+			default:
+				System.out.println("");
+				System.out.println("Closing the application.");
+			}
+		}
+		sc.close();
+	}
+}
---
+++ src/entities/Client.java
@@ -0,0 +1,78 @@
+package entities;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Client {
+
+	Scanner sc = new Scanner(System.in);
+	Random rd = new Random();
+	private final int bankAccount = rd.nextInt(99999);
+	private String clientName;
+	private double accountBalance;
+	
+
+	@Override
+	public String toString() {
+		return "Client Account: "
+				+ getBankAccount()
+				+ "\n"
+				+ "Client Name: "
+				+ getClientName()
+				+ "\n"
+				+ "Account Balance: $"
+				+ getAccountBalance();
+	}
+	
+	//Getters and Setter
+	
+	
+	public String getClientName() {
+		return clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	public int getBankAccount() {
+		return bankAccount;
+	}
+	
+	//Functions
+	
+	public void withdraw(double withdraw) {
+		this.accountBalance -= withdraw - 5.0;
+	}
+	
+	public void deposit(double deposit) {
+		this.accountBalance += deposit;
+	}
+	
+	public void register() {
+		char aux;
+		
+		System.out.println("Welcome to the Imaginary Bank.");
+		System.out.println("Let's register the account.");
+		System.out.println("Client name: ");		
+		setClientName(sc.nextLine());
+		
+		System.out.println("Will be a first deposit? Y/N");
+		aux = sc.next().charAt(0);
+		
+		if(aux == 'y' || aux == 'Y') {
+			System.out.println("Type the value of the deposit: ");
+			deposit(sc.nextDouble());
+			System.out.println("");
+			System.out.println(toString());
+		}else {
+			System.out.println("");
+			System.out.println("Welcome " + getClientName());
+			System.out.println(toString());
+		}
+	}
+}
